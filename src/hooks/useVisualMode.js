29d14504fc257c1525a46d1428432dfa7d4add92
Serah Sirohi <serahsirohi@gmail.com>
2020-09-08 20:38:07 -0700

    after building a hook

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -3,15 +3,21 @@ import {useState} from "react";
 export default function useVisualMode(initial) {
   const [history, setHistory] = useState([initial]);
 
-  const transition = function (newMode, replace) {
+  const transition = function (newMode, replace = false) {
     if (!replace) {
-      setHistory([...history, newMode]);
+      setHistory((prev) => [...prev, newMode]);
     } else {
-      setHistory([...history.slice(0, history.length - 1, newMode)]);
+      setHistory((prev) => [...prev.slice(0, prev.length - 1, newMode)]);
     }
   };
 
-  const back = function () {};
+  const back = function () {
+    if (history.length < 2) {
+      return;
+    }
+
+    setHistory((prev) => [...prev.slice(0, prev.length - 1)]);
+  };
 
   return {mode: history[history.length - 1], transition, back};
 }
