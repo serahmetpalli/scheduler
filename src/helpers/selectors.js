d3a7cd65c5ac318745528804524ef888b1d79875
Serah Sirohi <serahsirohi@gmail.com>
2020-09-08 17:38:42 -0700

    after completing w7d4a1

--- src/helpers/selectors.js
+++ src/helpers/selectors.js
@@ -9,49 +9,19 @@ export const getAppointmentsForDay = function (state, day) {
 
   const appointments = days.appointments.map((id) => state.appointments[id]);
 
-  console.log("appointments", appointments);
+  // console.log("state", state);
 
   return appointments;
 };
 
 export const getInterview = function (state, interview) {
+  console.log("state.interviewers", state.interviewers);
+
   if (!interview) {
     return null;
   }
-
-  // const interviewData = state.interviewers.find(
-  //   (i) => i.id === interviewers.id
-  // );
-
-  for (const apt in state.appointments) {
-    if (state.appointments[apt].interview.interviewer === interview.interviewer) {
-    }
-  }
+  return {
+    ...interview,
+    interviewer: state.interviewers[interview.interviewer],
+  };
 };
-
-//add interviewers data to the fake state object.
-
-/*
-
- "interviewer": {
-      "id": 1,
-      "name": "Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
-    }
-
-    */
-
-
-    /*
-
-APPOINTMENTS: 
-4: {
-  id: 4,
-  time: "3pm",
-  interview: {
-  student: "Archie Cohen",
-  interviewer: "name": "Sylvia Palmer",
-      "avatar": "https://i.imgur.com/LpaY82x.png"
-    }
-  }
-},
